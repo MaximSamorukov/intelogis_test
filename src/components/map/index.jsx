f8a93dbc5fb2f01909daf36c6fdd2e12fed48cf2
Maxim Samorukov <maxim.samorukov@gmail.com>
2023-07-20 20:37:17 +0300

    wip working with leaflet

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { routes } from "../../constants";
 
 import { getRoute } from "../../http-services";
 
 export const Map = () => {
+  const currentRoute = useSelector((state) => state.route);
   useEffect(() => {
     getRoute(routes[0]).then((data) => console.log(data));
   }, []);
+  const firstCoords = currentRoute.selectedCoords?.length
+    ? currentRoute.selectedCoords[0]
+    : { lat: 50, lng: 50 };
   return (
     <MapContainer
       className="main"
-      center={[50.5, 50.5]}
-      zoom={3}
-      zoomControl={false}
+      center={firstCoords}
+      zoom={13}
+      zoomControl={true}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
